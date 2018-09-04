3c9cae8144cb1fc736107b0f01341afeb5660f75
Ziqing <42797628+ZiqingTan@users.noreply.github.com>
2018-09-04 09:18:23 +0800

    Update photos.py

--- baiduphoto/baiduphoto/spiders/photos.py
+++ baiduphoto/baiduphoto/spiders/photos.py
@@ -9,7 +9,8 @@ class PhotosSpider(Spider):
     name = 'photos'
     allowed_domains = ['http://image.baidu.com/']
     start_urls = ['http://image.baidu.com//']
-    phootos_url = 'http://image.baidu.com/search/index?tn=baiduimage&word=%E5%A3%81%E7%BA%B8&pn={next}&width=1920&height=1080'
+    headline = "壁纸"
+    phootos_url = 'http://image.baidu.com/search/index?tn=baiduimage&word='+ headline +'&pn={next}&width=1920&height=1080'
     def start_requests(self):
         for num in range(1, 180):
             yield Request (self.phootos_url.format(next=num*10),callback = self.parse_url)
